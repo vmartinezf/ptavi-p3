49dcce461c8e6ab80fcc543d9796f752fae08c13
vmartinezf <vanessa_1895@msn.com>
2015-10-14 17:15:12 +0200

    nuevo small

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -17,79 +17,81 @@ class SmallSMILHandler(ContentHandler):
         self.width = ""
         self.height = ""
         self.backgroundcolor = ""
-        self.region = ""
-        self.inRegion = 0
-        self.img = ""
-        self.inImg = 0
-        self.audio = ""
-        self.inAudio = 0
-        self.textstream = ""
-        self.inTextstream = 0
+        self.id = ""
+        self.top = ""
+        self.bottom = ""
+        self.left = ""
+        self.right = ""
+        self.src = ""
+        self.imgregion = ""
+        self.begin = ""
+        self.dur = ""
+        self.lista_etiquetas =  []
 
-    def get_tags(self, name, lista_etiquetas, atributo):
+        #self.dic = {
+         #   'root-layout': ['width', 'height'] }
+
+    def startElement(self, name, attrs):
         """
         Método que se llama para alamacenar las etiquetas,
         los atributos y su contenido
         """
-        lista_etiquetas.append = ('')
 
-    def startElement(self, name, attrs):
-        """
-        Método que se llama cuando se abre una etiqueta
-        """
+        #if name == 'root-layout':
+         #   for item in self.dic[name]:
+                # guardo en un dic..
+          #      dicrootlayout = {name : self.dic[name]}
+                
+
         if name == 'root-layout':
-            # De esta manera tomamos los valores de los atributos
-            self.width = attrs.get('width', "")  # FALTA ALGO
+            self.width = attrs.get('width', "")
             self.height = attrs.get('height', "")
-            self.backgroundcolor = attrs.get('backgroundcolor', "")
-            print(self.width)
-            print (self.backgroundcolor)
+            self.backgroundcolor = attrs.get('background-color', "")
+            dicrootlayout = {'width': self.width, 'height': self.height, 'background-color' : self.backgroundcolor}
+            rootlayout = {'root-layout' : dicrootlayout}
+            self.lista_etiquetas.append(rootlayout) 
         elif name == 'region':
-            self.inRegion = 1
+            self.id = attrs.get('id', "")
+            self.top = attrs.get('top', "")
+            self.bottom = attrs.get('bottom', "")
+            self.left = attrs.get('left', "")
+            self.right = attrs.get('right', "")
+            dicregion = {'id': self.id, 'top': self.top, 'bottom': self.bottom, 'left': self.left, 'right': self.right}
+            region = {'region': dicregion}
+            self.lista_etiquetas.append(region) 
         elif name == 'img':
-            self.inImg = 1
+            self.src = attrs.get('src', "")
+            self.region = attrs.get('region', "")
+            self.begin = attrs.get('begin', "")
+            self.dur = attrs.get('dur', "")
+            dicimg = {'src': self.src, 'region': self.region, 'begin': self.begin, 'dur': self.dur}
+            self.lista_etiquetas.append(dicimg)       
         elif name == 'audio':
+            self.src = attrs.get('src', "")
+            self.begin = attrs.get('begin', "")
+            self.dur = attrs.get('dur', "")
+            dicaudio = {'src': self.src, 'begin': self.begin, 'dur': self.dur}
+            self.lista_etiquetas.append(dicaudio)
             self.inAudio = 1
         elif name == 'textstream':
-            self.inTextstream = 1
+            self.src = attrs.get('src', "")
+            self.region = attrs.get('region', "")
+            dictextstream = {'src': self.src, 'region': self.region}
+            self.lista_etiquetas.append(dictextstream)
 
-    def endElement(self, name):
+    def get_tags (self):
         """
-        Método que se llama al cerrar una etiqueta
+        Método que devuelve las etiquetas,
+        los atributos y su contenido
         """
-        if name == 'region':
-            self.region = ""
-            self.inRegion = 0
-        if name == 'img':
-            self.img = ""
-            self.inImg = 0
-        if name == 'audio':
-            self.audio = ""
-            self.inImg = 0
-        if name == 'textstream':
-            self.textstream = ""
-            self.inTextstream = 0
-
-    def characters(self, char):
-        """
-        Método para tomar contenido de la etiqueta
-        """
-        if self.inRegion:
-            self.region = self.region + char
-        if self.inImg:
-            self.img = self.img + char
-        if self.inAudio:
-            self.audio = self.audio + char
-        if self.inTextstream:
-            self.textstream = self.stream + char
-
+        return self.lista_etiquetas
 
 if __name__ == "__main__":
     """
     Programa principal
     """
-    lista_etiquetas = []
     parser = make_parser()
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
+    print (cHandler.get_tags())
