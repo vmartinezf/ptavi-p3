aad81c88e9f2ea9ae78973c0f07757d3139cce28
vmartinezf <vanessa_1895@msn.com>
2015-10-14 19:18:18 +0200

    karaoke prueba

--- karaoke.py
+++ karaoke.py
@@ -2,16 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
-
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
+
+
+def imprimir(lista_etiquetas):
+    for dic in lista_etiquetas:
+        for elemento in dic:
+    #        print(elemento)
+  #          print (dic[elemento])
+            for dicelement in dic[elemento]:
+ #               print (dicelement)
+                x = dic[elemento]
+#                print (x[dicelement])
+                if (x[dicelement] != ""):
+                    linea = '\t' + dicelement + '=' + '"' + x[dicelement]+ '"'
+                
+     
 
 
 if __name__ == "__main__":
     try:
         fichero = sys.argv[1]
-        fich = open(fichero, 'r')
-        fich.close()
+        parser = make_parser()
+        cHandler = SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(fichero))
+        imprimir(cHandler.lista_etiquetas)
     except IndexError:
         sys.exit("Usage: python3 karaoke.py file.smil.")
     except IOError:
