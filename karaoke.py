92866f13a87bdf0b9c922f23d5deddde8d502961
vmartinezf <vanessa_1895@msn.com>
2015-10-15 18:51:47 +0200

    karaoke mejor

--- karaoke.py
+++ karaoke.py
@@ -7,30 +7,40 @@ from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
 
 
-def imprimir(lista_etiquetas):
-    for dic in lista_etiquetas:
-        for elemento in dic:
-    #        print(elemento)
-  #          print (dic[elemento])
-            for dicelement in dic[elemento]:
- #               print (dicelement)
-                x = dic[elemento]
-#                print (x[dicelement])
-                if (x[dicelement] != ""):
-                    linea = '\t' + dicelement + '=' + '"' + x[dicelement]+ '"'
-                
-     
+def guardar_linea_atributos(dic, line):
+    for elemento in dic:
+        line = line + elemento    
+        for dicelement in dic[elemento]:
+            x = dic[elemento]
+            if (x[dicelement] != ""):
+                linea = '\t' + dicelement + '=' + '"' + x[dicelement]+ '"'
+                line = line + linea
+    return line
+    
 
+def guardar_linea(lista_etiquetas, line):
+    for dic in lista_etiquetas:
+        line = guardar_linea_atributos(dic, line)
+        line = line + '\n'
+    return line
+    
+
+def imprimir(lista_etiquetas):
+    line = ""
+    linea = guardar_linea(lista_etiquetas, line)
+    print (linea)
+     
 
 if __name__ == "__main__":
     try:
         fichero = sys.argv[1]
+    except IndexError:
+        sys.exit("Usage: python3 karaoke.py file.smil.")
+    try:
         parser = make_parser()
         cHandler = SmallSMILHandler()
         parser.setContentHandler(cHandler)
         parser.parse(open(fichero))
-        imprimir(cHandler.lista_etiquetas)
-    except IndexError:
-        sys.exit("Usage: python3 karaoke.py file.smil.")
     except IOError:
         sys.exit("Usage: python3 karaoke.py file.smil.")
+    imprimir(cHandler.lista_etiquetas)
