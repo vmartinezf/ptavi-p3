27c643c929c9ed345c2cd6ff1829935ffc6532dc
vmartinezf <vanessa_1895@msn.com>
2015-10-18 08:50:37 +0200

    karaoke.py

--- karaoke.py
+++ karaoke.py
@@ -15,6 +15,7 @@ class KaraokeLocal(SmallSMILHandler):
         """
         Parseará el fichero y obtendrá etiquetas
         """
+        self.fichero = fichero
         parser = make_parser()
         cHandler = SmallSMILHandler()
         parser.setContentHandler(cHandler)
@@ -34,25 +35,23 @@ class KaraokeLocal(SmallSMILHandler):
             line = line + '\n'
         print (line)
 
-    def to_json(self, fichero, name):
+    def to_json(self, name):
         lista_etiq_json = json.dumps(self.lista_etiq)
-        if name == None :
-            name = 'karaoke.py'
         with open(name, 'w') as fichero_json:
             json.dump(lista_etiq_json, fichero_json, sort_keys=True, indent=4)
 
     def do_local(self):
         for dic in self.lista_etiq:
-           for elemento in dic:
-              for dicele in dic[elemento]:
-                 x = dic[elemento]
-                 if (x[dicele] != ""):
-                    if (dicele == "src") and (x[dicele] != "cancion.ogg"):
-                        URL = x[dicele]
-                        filename = URL[URL.rfind("/") + 1:]
-                        data = urlretrieve(URL, filename)
-                        urlcleanup()
-                        x[dicele] = "http://" + data[0]
+            for elemento in dic:
+                for dicele in dic[elemento]:
+                    x = dic[elemento]
+                    if (x[dicele] != ""):
+                        if (dicele == "src") and (x[dicele] != "cancion.ogg"):
+                            URL = x[dicele]
+                            filename = URL[URL.rfind("/") + 1:]
+                            data = urlretrieve(URL, filename)
+                            urlcleanup()
+                            x[dicele] = "http://" + data[0]
 
 
 if __name__ == "__main__":
@@ -65,7 +64,7 @@ if __name__ == "__main__":
     except IOError:
         sys.exit("Usage: python3 karaoke.py file.smil.")
     obj_karaokelocal.__str__()
-    obj_karaokelocal.to_json(fichero)
+    obj_karaokelocal.to_json('karaoke.json')
     obj_karaokelocal.do_local()
-    obj_karaokelocal.to_json(fichero, 'local.json')
+    obj_karaokelocal.to_json('local.json')
     obj_karaokelocal.__str__()
